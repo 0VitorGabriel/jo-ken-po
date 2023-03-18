704a1503438e2a6b20d3f259b8a175c173bf036e
0VitorGabriel <victoropo64@outlook.com>
2023-03-18 08:26:18 -0400

    possibilidades da tesoura

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,8 @@
 let user = document.querySelectorAll('input')
 let submit = document.querySelector('input#submit')
 let computer = document.querySelector('div#result')
-const store_computer = ['pedra', 'papel', 'tesoura']
 
+const store_computer = ['pedra', 'papel', 'tesoura']
 
 function stone_possibilities(random) {
     if (user[0].checked && store_computer[random] == 'pedra') {
@@ -24,13 +24,23 @@ function paper_possibilities(random) {
     }
 }
 
+function scissors_possibilities(random) {
+    if (user[2].checked && store_computer[random] == 'tesoura') {
+        return alert(`${store_computer[random]}: empate`)
+    } else if (user[2].checked && store_computer[random] == 'pedra') {
+        return alert(`${store_computer[random]}: perdeu`)
+    } else if (user[2].checked && store_computer[random] == 'papel') {
+        return alert(`${store_computer[random]}: ganhou`)
+    }
+}
+
 submit.addEventListener('click', function finaly() {
+
     let random_store_computer = Math.floor(
         Math.random() * store_computer.length
     )
-
     
     stone_possibilities(random_store_computer)
     paper_possibilities(random_store_computer)
+    scissors_possibilities(random_store_computer)
 }) 
-
