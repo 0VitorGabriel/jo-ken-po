836ee773724c260741dba9504351f3cf0c956978
0VitorGabriel <victoropo64@outlook.com>
2023-03-19 09:59:23 -0400

    adicionei um sistema de pontos

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,6 @@
 const user = document.querySelectorAll('input')
 const submit = document.querySelector('input#submit')
+
 let computer = document.querySelector('div#result')
 
 const store_computer = ['pedra', 'papel', 'tesoura']
@@ -7,41 +8,44 @@ const store_computer = ['pedra', 'papel', 'tesoura']
 const img = document.querySelector('img#computer')
 const h1 = document.querySelector('h1#result')
 
+let point = 0
+let area_point = document.querySelector('p#point')
+
 function stone_possibilities(random) {
     if (user[0].checked && store_computer[random] == 'pedra') {
 
-        return img.src = '../imgs/pedra.png', h1.innerHTML = 'empate'
+        return img.src = '../imgs/pedra.png', h1.innerHTML = 'empate', point += 0, area_point.innerHTML = `pontos: ${point}`
     } else if (user[0].checked && store_computer[random] == 'papel') {
         
-        return img.src = '../imgs/papel.png', h1.innerHTML = 'perdeu!'
+        return img.src = '../imgs/papel.png', h1.innerHTML = 'perdeu!', point -= 1, area_point.innerHTML = `pontos: ${point}`
     } else if (user[0].checked && store_computer[random] == 'tesoura') {
-        return img.src = '../imgs/tesoura.png', h1.innerHTML = 'ganhou!'
+        return img.src = '../imgs/tesoura.png', h1.innerHTML = 'ganhou!', point += 1, area_point.innerHTML = `pontos: ${point}`
     }
 }
 
 function paper_possibilities(random) {
     if (user[1].checked && store_computer[random] == 'papel') {
 
-        return img.src = '../imgs/papel.png', h1.innerHTML = 'empate'
+        return img.src = '../imgs/papel.png', h1.innerHTML = 'empate', point += 0, area_point.innerHTML = `pontos: ${point}`
     } else if (user[1].checked && store_computer[random] == 'tesoura') {
 
-        return img.src = '../imgs/tesoura.png', h1.innerHTML = 'perdeu!'
+        return img.src = '../imgs/tesoura.png', h1.innerHTML = 'perdeu!', point -= 1, area_point.innerHTML = `pontos: ${point}`
     } else if (user[1].checked && store_computer[random] == 'pedra') {
 
-        return img.src = '../imgs/pedra.png', h1.innerHTML = 'ganhou!'
+        return img.src = '../imgs/pedra.png', h1.innerHTML = 'ganhou!', point += 1, area_point.innerHTML = `pontos: ${point}`
     }
 }
 
 function scissors_possibilities(random) {
     if (user[2].checked && store_computer[random] == 'tesoura') {
 
-        return img.src = '../imgs/tesoura.png', h1.innerHTML = 'empate'
+        return img.src = '../imgs/tesoura.png', h1.innerHTML = 'empate', point += 0, area_point.innerHTML = `pontos: ${point}`
     } else if (user[2].checked && store_computer[random] == 'pedra') {
 
-        return img.src = '../imgs/pedra.png', h1.innerHTML = 'perdeu!'
+        return img.src = '../imgs/pedra.png', h1.innerHTML = 'perdeu!', point -= 1, area_point.innerHTML = `pontos: ${point}`
     } else if (user[2].checked && store_computer[random] == 'papel') {
 
-        return img.src = '../imgs/papel.png', h1.innerHTML = 'ganhou!'
+        return img.src = '../imgs/papel.png', h1.innerHTML = 'ganhou!', point += 1, area_point.innerHTML = `pontos: ${point}`
     }
 }
 
